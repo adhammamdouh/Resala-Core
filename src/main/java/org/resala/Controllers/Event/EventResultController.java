091e784db18712ec6248cb150682af37f1f7ff75
AhmedOsama <36333141+AhmedOsama123@users.noreply.github.com>
2021-07-10 06:42:36 +0200

    last stable one for kpi , event result

--- src/main/java/org/resala/Controllers/Event/EventResultController.java
+++ src/main/java/org/resala/Controllers/Event/EventResultController.java
@@ -3,6 +3,7 @@ package org.resala.Controllers.Event;
 import org.resala.Models.Auth.Response;
 import org.resala.Models.Event.EventResult;
 import org.resala.Service.Event.EventResultService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/eventResult")
 public class EventResultController {
+    @Autowired
     EventResultService eventResultSerivice;
     @RequestMapping(value = "/generateKPIs", method = RequestMethod.POST)
     public ResponseEntity<Object> generateKPIs() {
