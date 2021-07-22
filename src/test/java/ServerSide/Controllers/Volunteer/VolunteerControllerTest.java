4f2105cd87964a12b13fa36173879cb9caa55e98
AhmedOsama <36333141+AhmedOsama123@users.noreply.github.com>
2021-07-22 16:59:25 +0200

    add volunteer test cases

--- src/test/java/ServerSide/Controllers/Volunteer/VolunteerControllerTest.java
+++ src/test/java/ServerSide/Controllers/Volunteer/VolunteerControllerTest.java
@@ -85,9 +85,16 @@ public class VolunteerControllerTest extends AbstractTest {
         authorities.add(new SimpleGrantedAuthority(StaticNames.requestToArchiveVolunteer));
         authorities.add(new SimpleGrantedAuthority(StaticNames.acceptToArchiveVolunteer));
         authorities.add(new SimpleGrantedAuthority(StaticNames.declineToArchiveVolunteer));
+        authorities.add(new SimpleGrantedAuthority(StaticNames.createVolunteer));
         String token = jwtUtil.createTokenForTest(1, 1, authorities);
+        String json = "[{\"age\":\"21\",\"gender\":\"1\",\"address\":{\"capital\":{\"id\":\"1\"},\"streetName\":\"151b\",\"regionName\":\"xx3\",\"buildingNumber\":\"15\",\"apartmentNumber\":\"47\"},\"branch\":{\"id\":\"3\"},\"shirt\":{\"id\":\"1\"},\"faculty\":\"FCI\",\"nationalId\":\"2558844663311\",\"university\":\"Cairo\",\"firstName\":\"okasha\",\"midName\":\"okasha\",\"lastName\":\"okasha\",\"nickName\":\"okasha\",\"phoneNumber\":\"01529990900\",\"joinDate\":\"2020-15-12\",\"birthDate\":\"1999-08-23\",\"miniCamp\":\"false\",\"educationLevel\":{\"id\":1}}]";
+        mvc.perform(MockMvcRequestBuilders.post("/volunteer/add").header(HttpHeaders.AUTHORIZATION, "Bearer " + token)
+                .content(json)
+                .accept(getMediaTypeHeader())
+                .contentType(getMediaTypeHeader())).andExpect(status().isOk());
 
-        String jsonPhone = "{\"phoneNumber\":\"01529180900\"}";
+
+        String jsonPhone = "{\"phoneNumber\":\"01529990900\"}";
         MvcResult mvcVolunteerResult = mvc.perform(MockMvcRequestBuilders.post("/volunteer/getByPhoneNumber").header(HttpHeaders.AUTHORIZATION, "Bearer " + token)
                 .content(jsonPhone)
                 .accept(getMediaTypeHeader())
