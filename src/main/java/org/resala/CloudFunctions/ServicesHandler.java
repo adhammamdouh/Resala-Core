db7347548e9cb91468ff5caf4a5cd3137148eb57
AbdoOkasha <abdookasha01@gmail.com>
2021-07-25 08:32:14 +0200

    cloud testing

--- src/main/java/org/resala/CloudFunctions/ServicesHandler.java
+++ src/main/java/org/resala/CloudFunctions/ServicesHandler.java
@@ -3,16 +3,13 @@ package org.resala.CloudFunctions;
 import com.microsoft.azure.functions.ExecutionContext;
 import com.microsoft.azure.functions.annotation.FunctionName;
 import com.microsoft.azure.functions.annotation.TimerTrigger;
-import org.resala.Service.Volunteer.VolunteerService;
-import org.resala.dto.Volunteer.VolunteerDTO;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.function.adapter.azure.AzureSpringBootRequestHandler;
 
 public class ServicesHandler extends
         AzureSpringBootRequestHandler<String,String> {
 
-    @Autowired
-    VolunteerService volunteerService;
+//    @Autowired
+//    VolunteerService volunteerService;
 
 
     @FunctionName("leadVolunteerKPIGeneration1")
@@ -26,14 +23,14 @@ public class ServicesHandler extends
 
     @FunctionName("testing")
     public void test(
-            @TimerTrigger(name = "req12",schedule = "0 0-59 * * * *") String timerInfo,
+            @TimerTrigger(name = "req123",schedule = "0 0-59 * * * *") String timerInfo,
             final ExecutionContext context){
 
-        VolunteerDTO volunteerDTO=new VolunteerDTO();
-        volunteerDTO.setId(2);
-        String res= volunteerService.archive(volunteerDTO).getBody().toString();
+//        VolunteerDTO volunteerDTO=new VolunteerDTO();
+//        volunteerDTO.setId(2);
+//        String res= volunteerService.archive(volunteerDTO).getBody().toString();
 
-        context.getLogger().info("generating lead volunteer kpi "+res);
+        context.getLogger().info("generating lead volunteer kpi ");
 
     }
 
